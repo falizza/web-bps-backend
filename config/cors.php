cea212037957a7d0ea35672871c49b8e45f55ea5
Faliza Maulidina <falizamaulidina1508@gmail.com>
2025-07-14 00:19:59 +0700

    updated

--- config/cors.php
+++ config/cors.php
@@ -1,8 +1,12 @@
 <?php
+
 return [
-    'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'paths' => ['api/', 'login', 'logout', 'sanctum/csrf-cookie'],
+    'allowed_methods' => [''],
+    'allowed_origins' => [
+        'http://localhost:5173/', 
+        'https://web-bps-frontend-sepia.vercel.app/', 
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
