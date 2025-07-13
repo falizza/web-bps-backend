30319ff7e3c813e05da699547c97c8ffad9c69d1
Faliza Maulidina <falizamaulidina1508@gmail.com>
2025-07-14 00:03:44 +0700

    cors updated

--- config/cors.php
+++ config/cors.php
@@ -1,24 +1,11 @@
 <?php
-
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], // penting: pastiin login & auth route termasuk
-
-    'allowed_methods' => ['*'], // allow GET, POST, OPTIONS, PUT, DELETE, etc
-
-    'allowed_origins' => [], // dikosongin karena kita pakai regex di patterns
-
-    'allowed_origins_patterns' => [
-        '^https:\/\/.*\.vercel\.app$',      // semua subdomain vercel
-        '^https:\/\/.*\.dnmgroup\.my\.id$', // semua subdomain custom domain (kalau lo pake)
-        '^http:\/\/localhost:\d+$',         // buat dev lokal: localhost:5173 dll
-    ],
-
-    'allowed_headers' => ['*'], // biar semua header termasuk Authorization masuk
-
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 ];
